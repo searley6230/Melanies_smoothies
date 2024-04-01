844f7c34af278dd23522365ecdd9bb1136027409
searley6230 <165322702+searley6230@users.noreply.github.com>
2024-04-01 08:46:02 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,7 +21,7 @@ my_dataframe = session.table("smoothies.public.fruit_options").select (col('Frui
 
 import panda as pd
 
-pd_df=my_dataframe.to.pandas()
+pd_df=my_dataframe.to_pandas()
 st.dataframe(pd_df)
 st.stop
 
